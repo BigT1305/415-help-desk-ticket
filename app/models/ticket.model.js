6a1161abb50762161a556a79e97584688e8296f8
Anthony Hoss <BigT1305@users.noreply.github.com>
2019-05-07 21:12:35 -0500

    Updated model file

--- app/models/ticket.model.js
+++ app/models/ticket.model.js
@@ -1,20 +1,20 @@
-//The schema represents the fields of the JSON tickets.
 const mongoose = require('mongoose');
 
+//Create Ticket schema for JSON obects
 const TicketSchema = mongoose.Schema({
     id: Number,
-    created_at:  Date,
+    created_at: Date,
     updated_at: Date,
     type: String,
     subject: String,
     description: String,
     priority: String,
     recipient: String,
-    submitter: String,
     assignee_id: Number,
     follower_ids: Number,
-    tags: String,
+    tags
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Ticket', TicketSchema);
-
