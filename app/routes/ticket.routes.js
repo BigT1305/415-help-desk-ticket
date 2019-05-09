8643a4a4c76b30910555cd6ec6a1eded311003da
Anthony Hoss <BigT1305@users.noreply.github.com>
2019-05-08 19:29:28 -0500

    Add files via upload

--- app/routes/ticket.routes.js
+++ app/routes/ticket.routes.js
@@ -1,18 +1,18 @@
 module.exports = (app) => {
-    const ticket = require('../controllers/ticket.controller.js');
+    const tickets = require('../controllers/ticket.controller.js');
 
     //Endpoint to retrieve all tickets
-    app.get('/rest/list/', ticket.findAll);
+    app.get('/rest/list/', tickets.findAll);
 
     //Endpoint to get a single ticket based on id
-    app.get('/rest/ticket/:ticketId', ticket.findOne);
+    app.get('/rest/ticket/:ticketId', tickets.findOne);
 
     //Endpoint to create a new ticket based on id
-    app.post('/rest/ticket/', ticket.create);
+    app.post('/rest/ticket/', tickets.create);
 
     //Endpoint to delete a single ticket
-    app.delete('/rest/ticket/:ticketId', ticket.delete);
+    app.delete('/rest/ticket/:ticketId', tickets.delete);
 
     //Endpoint to update an existing ticket
-    app.put('/rest/ticket/:ticketId', ticket.update);
+    app.put('/rest/ticket/:ticketId', tickets.update);
 }
