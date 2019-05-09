00643ee5bb322ed95d515c4363af8dafc65173b5
Anthony Hoss <BigT1305@users.noreply.github.com>
2019-05-08 22:11:45 -0500

    Add files via upload

--- app/routes/ticket.routes.js
+++ app/routes/ticket.routes.js
@@ -7,8 +7,14 @@ module.exports = (app) => {
     //Endpoint to get a single ticket based on id
     app.get('/rest/ticket/:ticketId', tickets.findOne);
 
+    //Endpoint to get a single ticket returned as an XML document; should use the existing endpoint /rest/ticket/id to get the info as a JSON object, and return it in XML format
+    app.get('/rest/xml/ticket/:ticketId', ticket.module(findALl));
+
     //Endpoint to create a new ticket based on id
     app.post('/rest/ticket/', tickets.create);
+
+    //Ednpoint to add a single ticket send as an XML document
+    app.put('/rest/xml/ticket/:ticketId', ticket.module(create));
 
     //Endpoint to delete a single ticket
     app.delete('/rest/ticket/:ticketId', tickets.delete);
